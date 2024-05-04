aeb1062e5caebc58816b2c54947a97d1e1e7e8f0
kevin kirui <kipkoechkevin42@gmail.com>
2024-05-04 10:26:42 -0700

    changed file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD
 # Sets up a web server for deployment of web_static.
 
+# Update and install Nginx
 apt-get update
 apt-get install -y nginx
 
+# Create directories
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
+
+# Create a fake HTML file
 echo "Holberton School" > /data/web_static/releases/test/index.html
+
+# Create symbolic link
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
+# Give ownership to ubuntu
 chown -R ubuntu /data/
 chgrp -R ubuntu /data/
 
+# Update Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
 printf %s "server {
     listen 80 default_server;
     listen [::]:80 default_server;
@@ -36,32 +43,6 @@ printf %s "server {
     }
 }" > /etc/nginx/sites-available/default
 
+# Restart Nginx
 service nginx restart
-=======
-# script that sets up web servers for the deployment of web_static
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
-
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
-
-sudo chown -R ubuntu:ubuntu /data/
-
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
-
-sudo service nginx restart
->>>>>>> 2d7c8015fb661e303bc0730c3cf0b9354d29c688
