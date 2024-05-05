d4224b81172d5f15668ed7cb07229d1a05438804
kevin kirui <kipkoechkevin42@gmail.com>
2024-05-05 09:41:47 -0700

    -a

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,47 +1,21 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-# Fabfile to generates a .tgz archive from the contents of web_static.
-import os.path
-from datetime import datetime
-from fabric.api import local
-
-
-def do_pack():
-    """Create a tar gzipped archive of the directory web_static."""
-    dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-            dt.month,
-            dt.day,
-            dt.hour,
-            dt.minute,
-            dt.second)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
-        return None
-    return file
-=======
 """
-Fabric script to genereate tgz archive
+Fabric script to generate a .tgz archive
 execute: fab -f 1-pack_web_static.py do_pack
 """
 
 from datetime import datetime
-from fabric.api import *
-
+from fabric.api import local
 
 def do_pack():
     """
-    making an archive on web_static folder
+    Create a .tgz archive from the contents of the web_static folder.
     """
-
     time = datetime.now()
     archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
     local('mkdir -p versions')
     create = local('tar -cvzf versions/{} web_static'.format(archive))
-    if create is not None:
+    if create.succeeded:
         return archive
     else:
         return None
->>>>>>> 2d7c8015fb661e303bc0730c3cf0b9354d29c688
