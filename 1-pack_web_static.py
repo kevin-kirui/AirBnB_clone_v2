7d97cdbc29fd13818f274af773783b0ef04f158e
kevin kirui <kipkoechkevin42@gmail.com>
2024-05-06 10:06:12 -0700

    changed file 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,21 @@
-#!/usr/bin/python3
-"""
-Fabric script to generate a .tgz archive
-execute: fab -f 1-pack_web_static.py do_pack
-"""
-
-from datetime import datetime
 from fabric.api import local
+from datetime import datetime
+import os
 
 def do_pack():
-    """
-    Create a .tgz archive from the contents of the web_static folder.
-    """
-    time = datetime.now()
-    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
-    local('mkdir -p versions')
-    create = local('tar -cvzf versions/{} web_static'.format(archive))
-    if create.succeeded:
-        return archive
-    else:
+    """Generates a .tgz archive from the contents of the web_static folder."""
+    # create the versions directory if it doesn't exist
+    if not os.path.exists("versions"):
+        os.mkdir("versions")
+
+    # generate the file name in the format web_static_<year><month><day><hour><minute><second>.tgz
+    file_name = "versions/web_static_{}.tgz".format(datetime.now().strftime("%Y%m%d%H%M%S"))
+
+    # use the tar command to create a .tgz archive of the web_static directory
+    result = local("tar -cvzf {} web_static".format(file_name))
+
+    # return the archive path if the archive was correctly generated, otherwise return None
+    if result.failed:
         return None
+    else:
+        return file_name
